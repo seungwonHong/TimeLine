3e5f065feda1c198ab49f7354f20088b34b90869
seungwonHong <nonamed814@gmail.com>
2025-10-30 17:52:34 +0900

    fix: journey 레이아웃 버그 수정

--- src/features/landing/components/Journey.tsx
+++ src/features/landing/components/Journey.tsx
@@ -2,18 +2,26 @@ import React from "react";
 
 const Journey = () => {
   return (
-    <section id="section-2" className="2xl:w-[160rem] lg:w-[120rem] md:w-[56rem] w-[37rem] mx-auto 2xl:mt-[20rem] lg:mt-[14rem] md:mt-[10rem] mt-[12rem]">
-      <h2 className="2xl:text-[4.8rem] lg:text-[3.2rem] text-[2.8rem] font-normal 2xl:w-[47rem] lg:w-[32rem] md:w-[24rem] w-[30rem] mx-auto text-center" data-aos="fade-up">
+    <section
+      id="section-2"
+      className="2xl:w-[160rem] lg:w-[120rem] md:w-[56rem] w-[37rem] mx-auto 2xl:mt-[20rem] lg:mt-[14rem] md:mt-[10rem] mt-[12rem]"
+    >
+      <h2
+        className="2xl:text-[4.8rem] lg:text-[3.2rem] text-[2.8rem] font-normal 2xl:w-[47rem] lg:w-[32rem] md:w-[28rem] w-[30rem] mx-auto text-center"
+        data-aos="fade-up"
+      >
         The complete journey for developers
       </h2>
 
       <div className="grid lg:grid-cols-3 grid-cols-1 2xl:gap-[2.4rem] lg:gap-[2rem] gap-[6.4rem] w-full 2xl:mt-[6.4rem] lg:mt-[4rem] md:mt-[2.4rem] mt-[1.6rem]">
         <div className="flex flex-col w-full " data-aos="fade-up">
-          <img
-            src="/images/Communicate.png"
-            alt="Communicate"
-            className="w-full h-full object-cover md:aspect-square aspect-[16/9] rounded-[1.6rem]"
-          />
+          <div className="w-full md:aspect-square aspect-[16/9] rounded-[1.6rem] overflow-hidden">
+            <img
+              src="/images/Communicate.png"
+              alt="Communicate"
+              className="w-full h-full object-cover"
+            />
+          </div>
           <span className="2xl:text-[3.2rem] md:text-[2.4rem] text-[2rem] font-normal">
             Communicate <span className="text-[#808080]">with developers</span>
           </span>
@@ -23,11 +31,13 @@ const Journey = () => {
           </span>
         </div>
         <div className="flex flex-col w-full " data-aos="fade-up">
-          <img
-            src="/images/Collaborate.png"
-            alt="Collaborate"
-            className="w-full h-full object-cover md:aspect-square aspect-[16/9] rounded-[1.6rem]"
-          />
+          <div className="w-full md:aspect-square aspect-[16/9] rounded-[1.6rem] overflow-hidden">
+            <img
+              src="/images/Collaborate.png"
+              alt="Collaborate"
+              className="w-full h-full object-cover"
+            />
+          </div>
           <span className="2xl:text-[3.2rem] md:text-[2.4rem] text-[2rem] font-normal">
             Collaborate <span className="text-[#808080]">with developers</span>
           </span>
@@ -37,11 +47,13 @@ const Journey = () => {
           </span>
         </div>
         <div className="flex flex-col w-full " data-aos="fade-up">
-          <img
-            src="/images/Grow.png"
-            alt="Grow"
-            className="w-full h-full object-cover md:aspect-square aspect-[16/9] rounded-[1.6rem]"
-          />
+          <div className="w-full md:aspect-square aspect-[16/9] rounded-[1.6rem] overflow-hidden">
+            <img
+              src="/images/Grow.png"
+              alt="Grow"
+              className="w-full h-full object-cover"
+            />
+          </div>
           <span className="2xl:text-[3.2rem] md:text-[2.4rem] text-[2rem] font-normal">
             Grow <span className="text-[#808080]">with developers</span>
           </span>
